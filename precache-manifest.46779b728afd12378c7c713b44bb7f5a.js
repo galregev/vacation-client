c99d6514e7c224696397ad70b44906509d87646c
galregev <galregev55@gmail.com>
2019-05-09 19:29:14 +0300

    withput design

---
+++ precache-manifest.46779b728afd12378c7c713b44bb7f5a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e24f0a8aa7520a1c488a5293a1f3e3aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6cb47a157b9a3053d48e",
+    "url": "/static/css/main.6fecda3f.chunk.css"
+  },
+  {
+    "revision": "cb1558e34a08301f2f46",
+    "url": "/static/js/2.2728de24.chunk.js"
+  },
+  {
+    "revision": "6cb47a157b9a3053d48e",
+    "url": "/static/js/main.6c0390c4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
